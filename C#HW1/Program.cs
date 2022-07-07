43ac740f9415b146dd2610b72a5b1dbc449ab094
Naganishe <mr.nordakov@gmail.com>
2022-07-07 15:17:46 +0300

    add C#HW1 & C#HW2

---
+++ C#HW1/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задайте значение M и N. Напишите программу, которая выведет все натуральные числа
+// в промежутке от М до N.
+
+void PrintNaturalNumber (int M, int N)
+{
+    if (M <= N)
+    {
+        Console.WriteLine(M);
+        PrintNaturalNumber(M + 1, N);
+    }
+}
+
+Console.WriteLine("Введите число М: ");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+PrintNaturalNumber(M, N);
---
+++ C#HW2/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задайте значения M и N. Напишите программу которая найдет сумму
+// натуральных элементов в промежутке от М до N.
+
+int SumElements (int M, int N)
+{
+    int Stop = N;
+    int Sum = M;
+    if (M != Stop)
+    {
+        Sum += SumElements(M + 1, N);
+    }
+    return Sum;
+}
+
+Console.WriteLine("Введите число М: ");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+int result = SumElements(M, N);
+Console.WriteLine($"result = {result}");
