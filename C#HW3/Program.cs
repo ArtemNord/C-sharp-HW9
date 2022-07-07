2c4d9b06ffdc35555e965c969c9b08a246c85ead
Naganishe <mr.nordakov@gmail.com>
2022-07-07 16:25:57 +0300

    add C#HW3 Akkerman

--- C#HW3/Program.cs
+++ C#HW3/Program.cs
@@ -1,3 +1,19 @@
 ﻿// Напишите программу вычисления функции Аккермана с помощью рекурсии.
 // Даны два не отрицательных числа M и N.
 
+int Akkerman(int N, int M)
+{
+    if (N == 0)
+    {
+        return M + 1;
+    }
+    if (M == 0 && N > 0)
+    {
+        return Akkerman(N - 1, 1);
+    }
+    return Akkerman(N - 1, Akkerman(N, M - 1));
+}
+
+int result = Akkerman(3,2);
+
+Console.WriteLine(result);
